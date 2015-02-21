db9dbc2aab2118d3fd7fc199c143f6e19b54a501
Lee Fallat <me@leefallat.ca>
2015-02-21 08:17:05 -0500

    Apparently didn't do real rot13 (whoops!), redid

--- 04/dencrypt.hs
+++ 04/dencrypt.hs
@@ -1,14 +1,22 @@
 import Data.Char
 import System.Environment
 
-rot13 str = map (\x -> chr $ (+) (ord x) 13) str
-unrot13 str = map (\x -> chr $ (-) (ord x) 13) str
+rot13 = map add13
+add13 ch 
+	| isAlpha lch = (chr . add13_) lch
+	| otherwise = lch
+	where lch = toLower ch
+
+add13_ ch 
+	| ochr > oZ = (ochr - oZ) + ord 'a' - 1
+	| ochr <= oZ = ochr
+	where 
+		ochr = 13 + ord ch
+		oZ = ord 'z'
 
 main :: IO()
 main = do
 	args <- getArgs
 	putStrLn $ case args of
-		["0",str] -> unrot13 str
-		["1",str] -> rot13 str
-		[] -> "No parameters"
+		(str:_) -> rot13 str
 		otherwise -> "Invalid parameters"
