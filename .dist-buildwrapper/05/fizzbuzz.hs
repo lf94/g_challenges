f06424598b4fad8ce698d6ee6c2f7cfd8124a610
Lee Fallat <me@leefallat.ca>
2015-04-25 10:40:50 -0400

    More uselessness removed.

--- .dist-buildwrapper/05/fizzbuzz.hs
+++
@@ -1,8 +0,0 @@
-fizzBuzz :: Int -> String
-fizzBuzz n | n % 3 == 0 = "Fizz"
-           | n % 5 == 0 = "Buzz"
-           | n % 3 == 0 && n % 5 == 0 = "FizzBuzz"
-
-main :: IO()
-main = do
-    mapM_ print $ map fizzBuzz [1..100]
